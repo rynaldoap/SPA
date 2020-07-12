88ac3970bf26e9b95782abf5e8e156865b1fd915
rynalldo <aldo@premiro.com>
2020-07-12 23:05:33 +0700

    1

---
+++ src/Faq.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Faq extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Faq</h2>
+                <p>asdasdasdasdasd</p>
+            </div>
+        );
+    }
+}
